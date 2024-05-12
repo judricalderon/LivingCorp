f5b79cf5fc437996b8d2d0bc90265861ef75a2dc
judricalderon <judricalderon@gmail.com>
2024-05-12 16:56:02 -0500

    termino el metodo crearPropiedad en el servicio (sin prueba)

--- src/main/java/co/edu/unbosque/livingcorp/service/AdmiPropertiesService.java
+++ src/main/java/co/edu/unbosque/livingcorp/service/AdmiPropertiesService.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque.livingcorp.service;
 
+import co.edu.unbosque.livingcorp.exception.RepetedObjectException;
 import co.edu.unbosque.livingcorp.model.dto.PropertyDto;
 import co.edu.unbosque.livingcorp.model.entity.Property;
 import co.edu.unbosque.livingcorp.model.presistence.InterfaceDao;
@@ -24,7 +25,7 @@ public class AdmiPropertiesService implements Serializable {
         modelMapper = new ModelMapper();
     }
 
-    private void createProperties(PropertyDto propertiesDto) {
+    private boolean createProperties(PropertyDto propertiesDto) throws RepetedObjectException {
         List<PropertyDto> listaBd = propertyDao.getAll()
                                                 .stream()
                                                 .map(entity ->modelMapper.map(entity,PropertyDto.class))
@@ -33,10 +34,14 @@ public class AdmiPropertiesService implements Serializable {
             boolean nombreIgual = listaBd.stream()
                     .anyMatch(entity -> entity.getNameProperty().equals(propertiesDto.getNameProperty()) && entity.getCity().equals(propertiesDto.getCity()));
             if (nombreIgual){
-
+                return false;
             }else {
-
+                propertyDao.create(modelMapper.map(propertiesDto, Property.class));
+                return true;
             }
+        }else {
+            propertyDao.create(modelMapper.map(propertiesDto, Property.class));
+            return true;            
         }
 
     }
