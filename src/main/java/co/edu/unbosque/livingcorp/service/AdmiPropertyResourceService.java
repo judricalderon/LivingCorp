ef881be2994e47136d061a7658ba8bbdf376ffcb
judricalderon <judricalderon@gmail.com>
2024-05-13 12:00:06 -0500

    termino el servicio para los recursos de las propiedades

--- src/main/java/co/edu/unbosque/livingcorp/service/AdmiPropertyResourceService.java
+++ src/main/java/co/edu/unbosque/livingcorp/service/AdmiPropertyResourceService.java
@@ -1,10 +1,12 @@
 package co.edu.unbosque.livingcorp.service;
 
+import co.edu.unbosque.livingcorp.exception.RepetedObjectException;
 import co.edu.unbosque.livingcorp.model.dto.PropertyDto;
 import co.edu.unbosque.livingcorp.model.dto.PropertyResourceDto;
 import co.edu.unbosque.livingcorp.model.dto.ResourceDto;
 import co.edu.unbosque.livingcorp.model.dto.UserDto;
 import co.edu.unbosque.livingcorp.model.entity.Property;
+import co.edu.unbosque.livingcorp.model.entity.PropertyResource;
 import co.edu.unbosque.livingcorp.model.entity.Resource;
 import co.edu.unbosque.livingcorp.model.entity.User;
 import co.edu.unbosque.livingcorp.model.presistence.InterfaceDao;
@@ -26,6 +28,8 @@ public class AdmiPropertyResourceService implements Serializable {
     private InterfaceDao<Property,Integer> propertyDao;
     @Inject
     private InterfaceDao<Resource,String> resourceDao;
+    @Inject
+    private InterfaceDao<PropertyResource,Integer> propertyResourceDao;
 
     private ModelMapper modelMapper;
 
@@ -33,29 +37,65 @@ public class AdmiPropertyResourceService implements Serializable {
         modelMapper = new ModelMapper();
     }
 
-    public boolean createPropertyResource(PropertyResourceDto propertyResourceDto) {
-        return true;
+    public boolean createPropertyResource(PropertyResourceDto propertyResourceDto) throws RepetedObjectException {
+        if (propertyResourceDto != null){
+            propertyResourceDto.setProId(findPropertyByName(propertyResourceDto.getProId()));
+            propertyResourceDto.setResId(findResourceByName(propertyResourceDto.getResId()));
+            propertyResourceDao.create(modelMapper.map(propertyResourceDto,PropertyResource.class));
+            return true;
+        }
+        return false;
     }
 
 
-    public List<UserDto> ListUser(){
+
+    public List<String> ListNameUser(UserDto userDto){
+
         return userDao.getAll()
                        .stream()
                        .map(entity -> modelMapper.map(entity, UserDto.class))
+                .filter(UserDto::isPropertyAdmin)
+                .map(UserDto::getEmailUser)
                 .collect(Collectors.toList());
     }
 
-    public List<PropertyDto> ListProperty(){
-        return propertyDao.getAll()
+    public PropertyDto findPropertyByName(PropertyDto propertyDto ){
+        List<PropertyDto> propertiesDto = propertyDao.getAll()
                 .stream()
                 .map(entity -> modelMapper.map(entity, PropertyDto.class))
                 .collect(Collectors.toList());
+        return propertiesDto.stream()
+                .filter(entity -> propertyDto.getNameProperty().equals(entity.getNameProperty()))
+                .findFirst().orElseGet(PropertyDto::new);
     }
 
-    public List<ResourceDto> ListResource(){
+    public List<String> ListNameProperty(PropertyDto propertyDto){
+
+        return propertyDao.getAll()
+                .stream()
+                .map(entity -> modelMapper.map(entity, PropertyDto.class))
+                .map(PropertyDto::getNameProperty)
+                .collect(Collectors.toList());
+    }   
+    
+    
+    
+    public ResourceDto findResourceByName(ResourceDto resourceDto){
+        List<ResourceDto> resourcesDto = resourceDao.getAll()
+                .stream()
+                .map(entity -> modelMapper.map(entity, ResourceDto.class))
+                .collect(Collectors.toList());
+        return resourcesDto.stream()
+                .filter(entity -> resourceDto.getType().equals(entity.getType()))
+                .findFirst().orElseGet(ResourceDto::new);
+    }
+
+    public List<String> ListNameResource(ResourceDto resourceDto){
+
         return resourceDao.getAll()
                 .stream()
                 .map(entity -> modelMapper.map(entity, ResourceDto.class))
+                .map(ResourceDto::getType)
                 .collect(Collectors.toList());
     }
 
